81601216550547cf7dbf5ca203914b41ab563615
horpto <__singleton__@hackerdom.ru>
2015-08-30 01:50:46 +0500

    Add simple proxy

---
+++ src/proxy.js
@@ -0,0 +1,79 @@
+"strict"
+
+var http = require("http");
+var url = require("url");
+var net = require("net");
+var extend = require('util')._extend;
+
+module.exports.init = init;
+
+function init(options) {
+  var proxy = http.createServer(function (req, res) {
+    console.info("Connection event", req.url);
+    /*
+    host: A domain name or IP address of the server to issue the request to. Defaults to 'localhost'.
+    hostname: To support url.parse() hostname is preferred over host
+    port: Port of remote server. Defaults to 80.
+    localAddress: Local interface to bind for network connections.
+    socketPath: Unix Domain Socket (use one of host:port or socketPath)
+    method: A string specifying the HTTP request method. Defaults to 'GET'.
+    path: Request path. Defaults to '/'. Should include query string if any. E.G. '/index.html?page=12'. An exception is thrown when the request path contains illegal characters. Currently, only spaces are rejected but that may change in the future.
+    headers: An object containing request headers.
+    auth: Basic authentication i.e. 'user:password' to compute an Authorization header.
+    agent: Controls Agent behavior. When an Agent is used request will default to Connection: keep-alive. Possible values:
+        undefined (default): use global Agent for this host and port.
+        Agent object: explicitly use the passed in Agent.
+        false: opts out of connection pooling with an Agent, defaults request to Connection: close.
+    keepAlive: {Boolean} Keep sockets around in a pool to be used by other requests in the future. Default = false
+    keepAliveMsecs: {Integer} When using HTTP KeepAlive, how often to send TCP KeepAlive packets over sockets being kept alive. Default = 1000. Only relevant if keepAlive is set to true.
+    */
+    var origin_url = url.parse(req.url);
+    var origin_req = http.request({
+      method: req.method,
+      hostname: origin_url.hostname,
+      path: origin_url.path,
+      auth: origin_url.auth,
+      port: origin_url.port,
+      headers: req.headers,
+      keepAlive: req.headers['connection'] != 'close',
+    },  function (res1) {
+      console.info("Origin server response:", res1.statusCode, res1.headers);
+      res.writeHead(res1.statusCode, http.STATUS_CODES[res1.statusCode], res1.headers);
+      res1.pipe(res);
+      res.pipe(res1);
+    });
+    origin_req.on("error", function (err) {
+      console.error("Origin server request error", err.stack);
+    });
+    origin_req.end();
+  });
+
+  proxy.on("connect", function(req, cltSocket, head) {
+    // connect to an origin server
+    console.info("Connect", req.url);
+
+    var srvUrl = url.parse("http://" + req.url);
+    var srvSocket = net.connect(srvUrl.port, srvUrl.hostname, function() {
+      cltSocket.write("HTTP/1.1 200 Connection Established\r\n" +
+                      "Proxy-agent: Node-Proxy\r\n" +
+                      "\r\n");
+      srvSocket.write(head);
+      srvSocket.pipe(cltSocket);
+      cltSocket.pipe(srvSocket);
+    });
+  });
+
+  proxy.on("error", function(err, socket){
+    console.error("Proxy error:", err.stack);
+    socket.end();
+  });
+
+  proxy.on("close", function() {
+    console.info("Proxy server closing");
+  })
+
+  proxy.listen(options.port, function() {
+    console.info("Listen port:", options.port);
+  });
+
+}
